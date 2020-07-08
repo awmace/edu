cb26de3918d5be0868c7c7effd6f374f92ff9404
npf <2638274890@qq.com>
2020-07-09 02:09:07 +0800

    Initial commit

---
+++ edu_client/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Top></Top>
+        <Mid></Mid>
+        <Bot></Bot>
+    </div>
+</template>
+
+<script>
+    import Top from "./cpdd/Top";
+    import Mid from "./cpdd/Mid";
+    import Bot from "./cpdd/Bot";
+
+    export default {
+        name: "Home",
+        components: {
+            Top:Top,
+            Mid:Mid,
+            Bot:Bot
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ edu_client/src/components/cpdd/Mid.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-carousel height="720px" :interval="3000" arrow="always">
+        <el-carousel-item>
+            <img src="/static/image/03.png" alt="">
+        </el-carousel-item>
+        <el-carousel-item>
+            <img src="/static/image/03.png" alt="">
+        </el-carousel-item>
+        <el-carousel-item>
+            <img src="/static/image/03.png" alt="">
+        </el-carousel-item>
+    </el-carousel>
+</template>
+
+<script>
+    export default {
+        name: "Middle"
+    }
+</script>
+
+<style scoped>
+
+</style>
